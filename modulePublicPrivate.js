2cd7cab1fc97d60144b628b13e2ad6607b8bd665
McGarrity <jmcgarrity@C02F5SVKDF8X.local>
2013-05-01 11:16:32 -0400

    updated with strict wrapper and added _ for private

--- modulePublicPrivate.js
+++ modulePublicPrivate.js
@@ -1,16 +1,20 @@
-"use strict"; // TODO wrap properly.
-var functionUtils = (function(){
-  /* private `slice` method */
-  function slice(arrayLike, index) {
-    return Array.prototype.slice.call(arrayLike, index || 0);
-  }
-  return {
-    /* exposed, public `bind` method */
-    bind: function(fn, thisArg) {
-       var args = slice(arguments, 2);
-       return function() {
-         return fn.apply(thisArg, args.concat(slice(arguments)));
-       };
-    }
-  };
+(function(){
+	"use strict";
+	var functionUtils = (function(){
+	  //Private
+	  function _slice(arrayLike, index) {
+	    return Array.prototype.slice.call(arrayLike, index || 0);
+	  }
+
+	  return {
+	    //Public
+	    bind: function(fn, thisArg) {
+	       var args = _slice(arguments, 2);
+	       return function() {
+	          return fn.apply(thisArg, args.concat(slice(arguments)));
+	       };
+	    }
+	  };
+	})();	
 })();
+
